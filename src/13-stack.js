3caacd063d693a194970a3ea9d2a1e074038fc7d
Zmiter <ozmiter@gmail.com>
2021-04-11 23:14:25 +0300

    feat: add stack solution

--- src/13-stack.js
+++ src/13-stack.js
@@ -11,16 +11,23 @@
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new Error('Not implemented');
+  constructor() {
+    this.stack = [];
+  }
+
+  push(element) {
+    // throw new Error('Not implemented');
+    this.stack.push(element);
   }
 
   pop() {
-    throw new Error('Not implemented');
+    // throw new Error('Not implemented');
+    return this.stack.pop();
   }
 
   peek() {
-    throw new Error('Not implemented');
+    // throw new Error('Not implemented');
+    return this.stack[this.stack.length - 1];
   }
 }
 
